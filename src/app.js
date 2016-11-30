94f20e78878158adbbdf14c2ebd03abe203550ed
mahikero <federicoparassarmiento@yahoo.com>
2016-11-30 15:14:36 -0500

    up

--- src/app.js
+++ src/app.js
@@ -6,11 +6,17 @@ var express = require('express'),
 var app = express();
 
 app.get('/', function(req, res){
-    res.send("<h1>Hello World</h1>");
+    res.send("<h1>Hello World your</h1>");
 });
 
-app.get('/blog', function(req, res){
-    res.send(posts);
+app.get('/blog/:title?', function(req, res){
+    var title = req.params.title;
+    if (title === undefined) {
+        res.status(503);
+        res.send("under maintenance");
+    };
+    var post = posts[title];
+    res.send(post);
 });
 
 app.listen(3000, function(){
