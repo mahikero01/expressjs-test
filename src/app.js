cd7566d26fd33a96619e9bca8974f23551b74f6a
mahikero <federicoparassarmiento@yahoo.com>
2016-11-30 14:21:04 -0500

    up

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
 'use strict';
 
-var express = require('express');
+var express = require('express'),
+ posts = require('./mock/posts.json');
+
+var app = express();
+
+app.get('/', function(req, res){
+    res.send("<h1>Hello World</h1>");
+});
+
+app.get('/blog', function(req, res){
+    res.send(posts);
+});
+
+app.listen(3000, function(){
+    console.log("FrontEnd Server running in port 3000");
+});
